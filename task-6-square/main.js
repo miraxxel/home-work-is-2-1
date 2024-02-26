ed64ef44c2157c960516d6d6aef678c95d04383e
miraxxel <forremains@yandex.ru>
2024-02-26 11:33:04 +0300

    Практическая работа. Функции

---
+++ task-6-square/main.js
@@ -0,0 +1,11 @@
+// Вариант 5
+
+let Square = (a) => {
+        let result = a * a;
+        console.log(result);
+}
+let number = prompt("Введите число");
+
+console.log("Квадрат введённого числа:");
+Square(number);
+
